96ba26c56931b593b98a52e5a8b1093576760700
Dyanesh Dhawale <dyanixdhawale@gmail.com>
2022-08-17 22:41:48 +0530

    added scroller

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Blog, Footer } from "./components";
 import UpcomingEvents from "./components/UpcomingEvents";
-
+import Footer1 from "./components/Footer1";
+import './components/Footer1.css'
 function App() {
   return (
     <>
       <Blog />
       <UpcomingEvents />
+      <Footer1/>
       <Footer />
     </>
   );
---
+++ src/components/Footer1.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import './Footer1.css'
+const Footer = () => {
+  return (
+    <div class="slider">
+      <div class="slide-track">
+
+        <div class="slide">
+          <img src='https://picsum.photos/id/236/200/300'></img>
+        </div>
+        <div class="slide">
+          <img src='https://picsum.photos/id/238/200/300'></img>
+        </div>
+        <div class="slide">
+          <img src='https://picsum.photos/id/239/200/300'></img>
+        </div>
+        <div class="slide">
+          <img src='https://picsum.photos/id/240/200/300'></img>
+        </div>
+        <div class="slide">
+          <img src='https://picsum.photos/id/241/200/300'></img>
+        </div>
+        <div class="slide">
+          <img src='https://picsum.photos/id/242/200/300'></img>
+        </div>
+        <div class="slide">
+          <img src='https://picsum.photos/id/243/200/300'></img>
+        </div>
+        <div class="slide">
+          <img src='https://picsum.photos/id/244/200/300'></img>
+        </div>
+        <div class="slide">
+          <img src='https://picsum.photos/id/265/200/300'></img>
+        </div>
+
+
+
+        <div class="slide">
+          <img src='https://picsum.photos/id/255/200/300'></img>
+        </div>
+        <div class="slide">
+          <img src='https://picsum.photos/id/247/200/300'></img>
+        </div>
+        <div class="slide">
+          <img src='https://picsum.photos/id/248/200/300'></img>
+        </div>
+        <div class="slide">
+          <img src='https://picsum.photos/id/249/200/300'></img>
+        </div>
+        <div class="slide">
+          <img src='https://picsum.photos/id/250/200/300'></img>
+        </div>
+        <div class="slide">
+          <img src='https://picsum.photos/id/251/200/300'></img>
+        </div>
+        <div class="slide">
+          <img src='https://picsum.photos/id/252/200/300'></img>
+        </div>
+        <div class="slide">
+          <img src='https://picsum.photos/id/253/200/300'></img>
+        </div>
+        <div class="slide">
+          <img src='https://picsum.photos/id/254/200/300'></img>
+        </div>
+      </div>
+    </div>   
+  )
+}
+
+export default Footer
