e5fdb15ce7d4fe64b0eec1a285f65e0ecb3a97b6
Sourabh-Bharale <sourabhbharale80@gmail.com>
2022-08-31 10:48:25 +0530

    route achevements

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import  Footer  from "./components/Footer/Footer";
 import AboutUs from "./components/AboutUs/AboutUs";
 import Home from "./components/Home/Home";
+import Achievements from "./components/Achivements";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Routes>
       <Route exact path="/" element={<Home/>}/>
       <Route exact path="/about" element={<AboutUs/>}/>
+      <Route exact path="/achievements" element={<Achievements/>}/>
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
       <Footer />
